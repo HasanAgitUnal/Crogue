a51305ee5d6f1ed2c082485ef0d1eeaa701caff1
HasanAgitUnal <hasanagitunal@proton.me>
2026-09-10 15:44:46 +0300

    docs: fix _CONTAINER method parameters

--- crogue.d.lua
+++ crogue.d.lua
@@ -41,17 +41,17 @@
 --- A class contains sol2 container methods (https://sol2.readthedocs.io/en/v2.20.6/containers.html#container-operations)
 --- @class _CONTAINER<T>
 --- @field [integer] T
---- @field size fun(): integer                          Returns the number of elements in the container.
---- @field set fun(key: integer, value: T)              Sets a value at the given index. If value is nil, erases the element at that index. If index is size+1, inserts the value at the end.
---- @field at fun(key: integer): T                      Returns the value at the given index. For non‑random‑access containers, this may return multiple values.
---- @field get fun(key: integer): T                     Returns the value at the given index (same as `at`). For non‑random‑access containers, this may return multiple values.
---- @field find fun(target: T): boolean                 Finds a value in the container. (For sequence containers, this performs a linear search.)
---- @field erase fun(target: integer)                   Erases the element at the given index. (For sequence containers, the target is an index.)
---- @field insert fun(target: integer, value: T)        Inserts a value at the specified index. (For sequence containers, the target is an index.)
---- @field add fun(value: T)                            Adds a value to the end of the container.
---- @field clear fun()                                  Removes all elements from the container.
---- @field pairs fun(): fun(), _CONTAINER<T>, nil       Lua 5.2+ only; use c:pairs() in Lua 5.1/LuaJIT.
---- @field ipairs fun(): fun(), _CONTAINER<T>, integer  Lua 5.2+ only; use c:ipairs() in Lua 5.1/LuaJIT.
+--- @field size fun(self: _CONTAINER<T>): integer                               Returns the number of elements in the container.
+--- @field set fun(self: _CONTAINER<T>, key: integer, value: T)                 Sets a value at the given index. If value is nil, erases the element at that index. If index is size+1, inserts the value at the end.
+--- @field at fun(self: _CONTAINER<T>, key: integer): T                         Returns the value at the given index. For non‑random‑access containers, this may return multiple values.
+--- @field get fun(self: _CONTAINER<T>, key: integer): T                        Returns the value at the given index (same as `at`). For non‑random‑access containers, this may return multiple values.
+--- @field find fun(self: _CONTAINER<T>, target: T): boolean                    Finds a value in the container. (For sequence containers, this performs a linear search.)
+--- @field erase fun(self: _CONTAINER<T>, target: integer)                      Erases the element at the given index. (For sequence containers, the target is an index.)
+--- @field insert fun(self: _CONTAINER<T>, target: integer, value: T)           Inserts a value at the specified index. (For sequence containers, the target is an index.)
+--- @field add fun(self: _CONTAINER<T>, value: T)                               Adds a value to the end of the container.
+--- @field clear fun(self: _CONTAINER<T>, )                                     Removes all elements from the container.
+--- @field pairs fun(self: _CONTAINER<T>, ): fun(), _CONTAINER<T>, nil          Lua 5.2+ only; use c:pairs() in Lua 5.1/LuaJIT.
+--- @field ipairs fun(self: _CONTAINER<T>, ): fun(), _CONTAINER<T>, integer     Lua 5.2+ only; use c:ipairs() in Lua 5.1/LuaJIT.
 
 --- @alias _HOOK_EVENT "before_refresh" | "after_refresh" | "start" | "game_start" | "game_end" | "game_quit" | "reload" | "ending" | "draw" | "level_gen" | "die" | "key" | "level_up" | "slot" | "item" | "card_event" | "s_save" | "s_load" | "damage"
 
